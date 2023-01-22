00f54f0324ac874f1d42e05ace079b1a66e95e14
Vinay-M-01 <98087682+Vinay-M-01@users.noreply.github.com>
2023-01-22 14:36:40 +0530

    Service card CSS updates

--- components/layout/servicesCard.js
+++ components/layout/servicesCard.js
@@ -1,20 +1,95 @@
-import React from 'react'
+import React from 'react' 
+import styled from "styled-components";
+
+const Container = styled.div`
+.container{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  }
+
+  .card{
+    position: relative;
+    width: 12rem;
+    height: 9rem;
+    margin: 0.75rem;
+    object-fit: cover;
+    object-position: center;
+  }
+  .card-image{
+    position: absolute;
+    top: 0;
+    width: 12rem;
+    height: 7rem;
+    z-index: 10;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 500ms;
+    border-radius: 0.375rem; /* 6px */
+  }
+
+  .card:hover .card-image{
+    position: absolute;
+    transform: translate(-1.5rem, -1.5rem);
+  }
+
+  .image{
+    border-radius: 0.5rem;
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .service-container{
+    margin-left: auto;
+    margin-right: auto;
+    width: 10rem;
+    height: 3.5rem;
+    margin-top: 2rem;
+    object-fit: cover;
+    object-position: center;
+    resize: both;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 500ms;
+    border-radius: 0.375rem; /* 6px */
+    background-color: #e0e0e0;
+  }
+
+  .card:hover .service-container{
+    transform: translate(3.5rem, 3.5rem);
+  }
+
+  .service-card{
+    font-size: 0.875rem; /* 14px */
+    line-height: 1.25rem; /* 20px */
+    text-align: center;
+    cursor: pointer;
+    font-weight: 600;
+    }
+
+`
 
 const ServicesCard = ({ image, service }) => {
-  return (
-    <div className="relative flex justify-center items-center flex-wrap">
-      <div className="group relative w-48 h-36 m-3 object-cover object-center">
-        <div className="group-hover:-translate-y-6 group-hover:-translate-x-6 absolute top-0 w-48 h-28 z-10 transition-all duration-500	ease-in-out rounded-md">
-          <img src={image} alt="image" className="rounded-lg object-cover object-center w-full h-full" />
+  return ( 
+    <Container>
+    <div className="container">
+      <div className="card">
+        <div className="card-image">
+          <img src={image} alt="image" className="image" />
         </div>
-        <div className="group-hover:translate-y-14 group-hover:translate-x-14 w-40 h-15 mt-8 object-cover resize transition-all duration-500 ease-in-out bg-gray-400 rounded-md">
-          <div className="text-sm text-center cursor-pointer">
+        <div className="service-container">
+          <div className="service-card">
             {service}<br />
             <span>Click here</span>
           </div>
         </div>
       </div>
     </div>
+    </Container>
   )
 }
 
